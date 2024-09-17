fbb1f435b8ac6db132007a219bf8f67ffbc893cc
rajon-git <rajon.zhsust15@gmail.com>
2024-09-17 17:23:40 +0600

    doctor review and also add theke add appointment

--- docDetails.js
+++ docDetails.js
@@ -1,16 +1,16 @@
 const getparams = () => {
-    const param = new URLSearchParams(window.location.search).get("doctorId");
-    fetch(`https://testing-8az5.onrender.com/doctor/list/${param}`)
+  const param = new URLSearchParams(window.location.search).get("doctorId");
+  loadTime(param);
+  fetch(`https://testing-8az5.onrender.com/doctor/list/${param}`)
     .then((res) => res.json())
     .then((data) => displayDetails(data));
 
-    fetch(`https://testing-8az5.onrender.com/doctor/review/?doctor_id=${param}`)
+  fetch(`https://testing-8az5.onrender.com/doctor/review/?doctor_id=${param}`)
     .then((res) => res.json())
     .then((data) => doctorReview(data));
 };
 
 const doctorReview = (reviews) => {
-  console.log(reviews)
   reviews.forEach((review) => {
     const parent = document.getElementById("doc-details-review");
     const div = document.createElement("div");
@@ -28,9 +28,10 @@ const doctorReview = (reviews) => {
 };
 
 const displayDetails = (doctor) => {
-    const parent = document.getElementById("doc-details")
-    const div = document.createElement("div")
-    div.classList.add("doc-details-container");
+  console.log(doctor);
+  const parent = document.getElementById("doc-details");
+  const div = document.createElement("div");
+  div.classList.add("doc-details-container");
   div.innerHTML = `
     <div class="doctor-img">
     <img src=${doctor.image} alt="" />
@@ -61,6 +62,67 @@ const displayDetails = (doctor) => {
   </div>
     `;
   parent.appendChild(div);
-}
+};
 
-getparams()
+const loadTime = (id) => {
+  fetch(
+    `https://testing-8az5.onrender.com/doctor/availabletime/?doctor_id=${id}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((item) => {
+        const parent = document.getElementById("time-container");
+        const option = document.createElement("option");
+        option.value = item.id;
+        option.innerText = item.name;
+        parent.appendChild(option);
+      });
+      console.log(data);
+    });
+};
+
+const handleAppointment = () => {
+  const param = new URLSearchParams(window.location.search).get("doctorId");
+  const status = document.getElementsByName("status");
+  const selected = Array.from(status).find((button) => button.checked);
+  const symptom = document.getElementById("symptom").value;
+  const time = document.getElementById("time-container");
+  const selectedTime = time.options[time.selectedIndex];
+  const patient_id = localStorage.getItem("patient_id");
+  const info = {
+    appointment_type: selected.value,
+    appointment_status: "Pending",
+    time: selectedTime.value,
+    symptom: symptom,
+    cancel: false,
+    patient: patient_id,
+    doctor: param,
+  };
+
+  console.log(info);
+  fetch("https://testing-8az5.onrender.com/appointment/", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(info),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      window.location.href = `pdf.html?doctorId=${param}`;
+      // handlePdf();
+      // console.log(data);
+    });
+};
+
+const loadPatientId = () => {
+  const user_id = localStorage.getItem("user_id");
+
+  fetch(`https://testing-8az5.onrender.com/patient/list/?user_id=${user_id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      localStorage.setItem("patient_id", data[0].id);
+    });
+};
+
+loadPatientId();
+getparams();
+loadTime();
