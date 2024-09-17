c073ae6d3dd418f4394b7de27db954b1cd5eb3ff
rajon-git <rajon.zhsust15@gmail.com>
2024-09-17 12:59:22 +0600

    review dynamicaly loaded from api

--- app.js
+++ app.js
@@ -105,6 +105,29 @@ const loadSpecialization = () => {
     });
 };
 
+const loadReview = () => {
+  fetch("https://testing-8az5.onrender.com/doctor/review/")
+    .then((res) => res.json())
+    .then((data) => displayReview(data));
+};
+
+const displayReview = (reviews) => {
+  reviews.forEach((review) => {
+    const parent = document.getElementById("review-container");
+    const div = document.createElement("div");
+    div.classList.add("review-card");
+    div.innerHTML = `
+        <img src="./Images/girl.png" alt="" />
+            <h4>${review.reviewer}</h4>
+            <p>
+             ${review.body.slice(0, 100)}
+            </p>
+            <h6>${review.rating}</h6>
+        `;
+    parent.appendChild(div);
+  });
+};
+
 const handleSearch = ()=>{
   const value = document.getElementById("search").value;
   loadDoctors(value)
@@ -114,3 +137,4 @@ loadDoctors()
 loadServices();
 loadDesignation();
 loadSpecialization();
+loadReview();
